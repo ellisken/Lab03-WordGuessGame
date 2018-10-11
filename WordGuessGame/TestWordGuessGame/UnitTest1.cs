fb7868687ea486dc764db64229afc09e3a193790
ellisken <kse104@gmail.com>
2018-10-11 10:02:34 -0700

    setup repo and project files

---
+++ WordGuessGame/TestWordGuessGame/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using WordGuessGame;
+using Xunit;
+
+namespace TestWordGuessGame
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
