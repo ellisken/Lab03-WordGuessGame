730909b2be83be249d47de23b1475ce01af42d53
ellisken <kse104@gmail.com>
2018-10-11 10:38:42 -0700

    file creation testing done

--- WordGuessGame/TestWordGuessGame/UnitTest1.cs
+++ WordGuessGame/TestWordGuessGame/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using WordGuessGame;
 using Xunit;
 
@@ -6,10 +7,50 @@ namespace TestWordGuessGame
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        /* REQUIRED:
+         * Test file can be created
+         * Test file can be updated
+         * Test file can be deleted
+         * Test word can be added to file
+         * Test retrieve all words from file
+         * Test letter exists method valid and invalid
+         */
 
+        //Test successful file creation in same dir as Program.cs
+        [Fact]
+        public void TestFileCreation()
+        {
+            string path = "../../../testFile.txt";
+            string[] words = {
+                "Hello",
+                "World",
+                "I",
+                "am",
+                "file",
+            };
+
+            Program.WriteToOrCreateFile(path, words);
+
+            Assert.True(File.Exists(path));
+        }
+
+        //Test incorrect path results in exception during file creation/writing
+        [Fact]
+        public void TestFileCreationInvalidPath()
+        {
+            string path = "../../../testFile***";
+            string[] words = {
+                "Hello",
+                "World",
+                "I",
+                "am",
+                "not",
+                "here"
+            };
+
+            Exception exception = Record.Exception(() => Program.WriteToOrCreateFile(path, words));
+            Assert.NotNull(exception);
+            Assert.IsType<System.IO.IOException>(exception);
         }
     }
 }
--- WordGuessGame/WordGuessGame/Program.cs
+++ WordGuessGame/WordGuessGame/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.IO;
 
 namespace WordGuessGame
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Josie Cat's Word Guessing Game!");
+        }
+
+
+        /// <summary>
+        /// Creates a file with the given path and writes each word from
+        /// the array of words to the file on its own line
+        /// </summary>
+        /// <param name="path">The path where the file exists or should be created</param>
+        /// <param name="words">An array of words that should be added to the file</param>
+        public static void WriteToOrCreateFile(string path, string[] words)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                try
+                {
+                    for(int i=0; i < words.Length; i++)
+                    {
+                        sw.WriteLine(words[i]);
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
         }
     }
 }
